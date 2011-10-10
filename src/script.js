087c61d0209fcfcaffd0de2691dfe20936166a21
Martin Czygan <martin.czygan@gmail.com>
2011-10-10 02:45:31 +0200

    packaging

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 var text = $("b:contains(ISBN-13:)").parent().html();
 var isbn13_re = /.*([0-9]{3}-[0-9]{10})/g;
 var isbn13 = isbn13_re.exec(text)[1];
-$("body").prepend($("<div id='libradar' style='color: #767676; background: aliceblue; padding: 5px;'>" + isbn13 + "</div>"));
+$("body").prepend($("<div id='libradar'>Checking availability of " + isbn13 + " ...</div>"));
 
 var ub_url = "http://ubdbs.ub.uni-leipzig.de/libero/WebopacOpenURL.cls?ACTION=SEARCH&sid=LIBERO:UBL&searchby1=ISBN&TERM_1=" + isbn13;
 $.get(ub_url, function(data) {
@@ -12,9 +12,9 @@ $.get(ub_url, function(data) {
 				/ Einträge Suchergebnisseite.*/, "");
 	console.log(results);
 	if (results == undefined || results == null || results == "") {
-		$("#libradar").html("Dieses Medium gibt es nicht in der UB Leipzig.");	
+		$("#libradar").html("&#8709; &mdash; This book isn't available.");	
 	} else {
-		$("#libradar").html("<a style='text-decoration: none' href='" + ub_url + "'>&#9733; Dieses Medium in der UB Leipzig ...</a>");	
+		$("#libradar").html("<span id='libradar-star'>&#9733;</span> &mdash; <a href='" + ub_url + "'>Oh great, this book is available at the UBL ...</a>");	
 	}
 	
 });
