3ea397c66376c06a99c8e1548ed4fbda07c6ce09
Martin Czygan <martin.czygan@gmail.com>
2011-10-12 06:22:34 +0200

    docs + fresh crx

--- libradar/libradar.js
+++ libradar/libradar.js
@@ -9,7 +9,7 @@
 //
 // On GitHub: [github.com/miku/libradar](https://github.com/miku/libradar)
 // 
-// Dumb screenshot: [imgur.com/a/DTv3m](http://imgur.com/a/DTv3m)
+// Dumb screenshots: [imgur.com/a/DTv3m](http://imgur.com/a/DTv3m)
 // 
 // ----
 
